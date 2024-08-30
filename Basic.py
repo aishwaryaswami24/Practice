e00a1104e7f116d025e2c87c19fb7195981d7863
aishwarya <swamiaishwarya24@gmail.com>
2024-08-30 23:42:01 +0530

    page.locator

--- Basic.py
+++ Basic.py
@@ -5,7 +5,10 @@ with sync_playwright() as p:
     page=context.new_page()
     page.goto('https://playwright.dev/')
     page.wait_for_timeout(2000)
-    page.get_by_text('Get started').click()
+    #page.get_by_text('Get started').click()
+    #page.wait_for_selector('.gh-text').click()
+    page.locator('.gh-text').click()
+
     page.set_viewport_size({'height': 800,'width': 200})
     page.screenshot(path='basic.png',full_page=True)
     page.close()
