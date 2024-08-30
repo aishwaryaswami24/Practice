e24d9918226a8b3cb93afdd603fa496df2267633
aishwarya <swamiaishwarya24@gmail.com>
2024-08-30 23:11:11 +0530

    record_video_dir('./videos/basic.mp4')

--- Basic.py
+++ Basic.py
@@ -1,8 +1,9 @@
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser=p.chromium.launch(headless=False)
-    context=browser.new_context()
+    context=browser.new_context(record_video_dir='./videos/basic.mp4')
     page=context.new_page()
     page.goto('https://playwright.dev/')
     page.wait_for_timeout(2000)
+    context.close()
     browser.close()
