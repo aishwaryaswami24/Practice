4664eb37061d224ed8955eaa708d5668b44a02b6
aishwarya <swamiaishwarya24@gmail.com>
2024-08-30 23:16:31 +0530

    page.video.save_as('basic1.mp4')

--- Basic.py
+++ Basic.py
@@ -5,5 +5,7 @@ with sync_playwright() as p:
     page=context.new_page()
     page.goto('https://playwright.dev/')
     page.wait_for_timeout(2000)
+    page.close()
+    page.video.save_as('basic1.mp4')
     context.close()
     browser.close()
