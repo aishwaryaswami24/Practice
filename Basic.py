49ce63236d629329f9b843418e5b5458698ebb7b
aishwarya <swamiaishwarya24@gmail.com>
2024-08-31 22:57:35 +0530

    tracing

--- Basic.py
+++ Basic.py
@@ -1,12 +1,14 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-@pytest.mark.skip(reason='test is nt=ot ready')
-def test_browser_launch():
-    with sync_playwright() as p:
+# @pytest.mark.skip(reason='test is nt=ot ready')
+# def test_browser_launch():
+with sync_playwright() as p:
         browser=p.chromium.launch(headless=False)
         context=browser.new_context(record_video_dir='./videos/basic.mp4')
         page=context.new_page()
+        context.tracing.start(screenshots=True,snapshots=True,sources=True)
+
         page.goto('https://playwright.dev/')
         page.wait_for_timeout(2000)
 
@@ -19,5 +21,6 @@ def test_browser_launch():
         page.screenshot(path='basic.png',full_page=True)
         page.close()
         page.video.save_as('basic1.mp4')
+        context.tracing.stop(path='trace.zip')
         context.close()
         browser.close()
