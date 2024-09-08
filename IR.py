3fd5a98c7b0cc5700a21856cce94db61b7873782
aishwarya <swamiaishwarya24@gmail.com>
2024-09-08 19:14:47 +0530

    video,scrrenshot

--- IR.py
+++ IR.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser=p.chromium.launch(headless=False)
-    context=browser.new_context()
+    context=browser.new_context(record_video_dir='./video/irctc.webcm')
     page=context.new_page()
     page.goto('https://www.irctc.co.in/nget/train-search')
     page.wait_for_timeout(2000)
+    page.screenshot(path='./irctc.png')
     context.close()
     browser.close()
