66ef28a09cf0e5ace25f6eb0cae3985810e149b3
aishwarya <swamiaishwarya24@gmail.com>
2024-09-09 23:12:22 +0530

    dict1 | dict2

--- dict.py
+++ dict.py
@@ -1,2 +1,9 @@
 dict1={'ID': '101','Name': 'Aishwarya'}
 dict2={'ID':'102','Name': 'Shubham'}
+
+#when both dicts attribute names are same then it will replace dict1 attribute to dict2
+dict1.update(dict2)
+print(dict1)
+
+dict3=dict1 | dict2
+print(dict3)
