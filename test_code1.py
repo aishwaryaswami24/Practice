50c8e4d3e5356d9b1805d98f9f7fa033ddfbe65a
aishwarya <swamiaishwarya24@gmail.com>
2024-09-19 12:50:26 +0530

    webtable

--- test_code1.py
+++ test_code1.py
@@ -8,9 +8,9 @@ def test_web_table():
         #table_name=page.locator('//table[@class="tsc_table_s13"]//tbody//tr/td[2]').all_text_contents()
         # table_name=page.locator('//table[@id="customers"]//tbody//tr/td[1]').all_text_contents()
         table_name=page.locator('#customers tbody tr >> nth=1').all_text_contents()
-        for i in table_name:
-         print(i)
-        # print(table_name)
+        # for i in table_name:
+        #  print(i)
+        print("/n".join(table_name))
         page.wait_for_timeout(2000)
         browser.close()
 test_web_table()
