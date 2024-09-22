7807d9b2da2b7f3eebc3e65ecfc4654ddc2f3128
aishwarya <swamiaishwarya24@gmail.com>
2024-09-22 13:37:54 +0530

    webtable

--- test_code1.py
+++ test_code1.py
@@ -1,17 +1,11 @@
 from playwright.sync_api import sync_playwright
-def test_web_table():
+
+def test_webtable():
     with sync_playwright() as p:
-        browser=p.chromium.launch(headless=False,slow_mo=500)
-        context=browser.new_context()
-        page=context.new_page()
-        page.goto('https://www.techlistic.com/2017/02/automate-demo-web-table-with-selenium.html')
-        #table_name=page.locator('//table[@class="tsc_table_s13"]//tbody//tr/td[2]').all_text_contents()
-        # table_name=page.locator('//table[@id="customers"]//tbody//tr/td[1]').all_text_contents()
-        #table_name=page.locator('#customers tbody tr >> nth=1').all_text_contents()
-        table_name = page.locator('#customers tbody tr:nth-child(2) td').all_text_contents()
-        # for i in table_name:
-        #  print(i)
-        print("\n".join(table_name))
+        browser=p.chromium.launch(headless=False)
+        page=browser.new_page()
+        page.goto('https://datatables.net/examples/basic_init/zero_configuration.html')
+        assert page.wait_for_selector('//table[@id="example"]').is_visible()
         page.wait_for_timeout(2000)
         browser.close()
-test_web_table()
+
