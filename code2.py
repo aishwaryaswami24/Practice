06f079ca02666078141c6f468d066fef1dc49b35
aishwarya <swamiaishwarya24@gmail.com>
2024-10-01 15:45:38 +0530

    return ' '.join(s.split()[::-1])

--- code2.py
+++ code2.py
@@ -13,3 +13,10 @@ print(remove_dup)
 lst=[0,9,8,7,3,8,9]
 unique_list=dict.fromkeys(lst)
 print(list(unique_list))
+
+#reverse word
+def reverse_word(s):
+    return ' '.join(s.split()[::-1])
+s='hello world'
+print('reverse_word:',reverse_word(s))
+
