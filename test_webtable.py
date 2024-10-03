8377e81d438f9e7d96e4a07b86c75d9c5003decc
aishwarya <swamiaishwarya24@gmail.com>
2024-10-03 23:39:56 +0530

    webtable

--- test_webtable.py
+++ test_webtable.py
@@ -5,23 +5,42 @@ def test_webtable():
         browser=p.chromium.launch(headless=False)
         page=browser.new_page()
         page.goto('https://datatables.net/examples/basic_init/zero_configuration.html')
-        # assert page.wait_for_selector('//table[@id="example"]').is_visible()
+        assert page.wait_for_selector('//table[@id="example"]').is_visible()
 
+#only headings in table
         # columns=page.locator('//table[@id="example"]//thead/tr/th').all_text_contents()
         # print(columns)
 
+#specific column
         # column_index=1
         # column_data=page.locator(f'//table[@id="example"]//tbody/tr/td[{column_index}]').all_text_contents()
         # print(column_data)
 
+#specific row
         # row_index=1
         # row_data=page.locator(f'//table[@id="example"]//tbody/tr[{row_index}]/td').all_text_contents()
         # print('\n',row_data)
 
-        column_index=3
-        row_index=1
-        specific_rc=page.locator(f'//table[@id="example"]//tbody/tr[{row_index}]/td[{column_index}]').all_text_contents()
-        print(specific_rc)
+#specific row and column
+        # column_index=3
+        # row_index=1
+        # specific_rc=page.locator(f'//table[@id="example"]//tbody/tr[{row_index}]/td[{column_index}]').all_text_contents()
+        # print(specific_rc)
+
+#find out count of all rows
+        table=page.wait_for_selector('//table[@id="example"]')
+        tr=table.query_selector_all('tr')
+        print(len(tr))
+
+#find out count of all headings
+        table = page.wait_for_selector('//table[@id="example"]')
+        th = table.query_selector_all('th')
+        print(len(th))
+
+# find out count of all data
+        table = page.wait_for_selector('//table[@id="example"]')
+        td = table.query_selector_all('td')
+        print(len(td))
 
         page.wait_for_timeout(2000)
         browser.close()
